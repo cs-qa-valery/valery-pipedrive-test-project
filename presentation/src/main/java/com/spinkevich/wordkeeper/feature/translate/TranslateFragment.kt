a0bb8fdc073d6bf273b054d4b4b6fa68885ed349
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2019-11-03 23:03:41 +0300

    choose language, handling errors

--- presentation/src/main/java/com/spinkevich/wordkeeper/feature/translate/TranslateFragment.kt
+++ presentation/src/main/java/com/spinkevich/wordkeeper/feature/translate/TranslateFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
+import com.spinkevich.data.utils.LanguageHelper
 import com.spinkevich.domain.model.Translation
 import com.spinkevich.wordkeeper.BaseApp
 import com.spinkevich.wordkeeper.R
@@ -15,7 +17,10 @@ import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
 
-class TranslateFragment : BaseFragment(), KodeinAware {
+private const val FROM_LANGUAGE_CODE = 21
+private const val TO_LANGUAGE_CODE = 22
+
+class TranslateFragment : BaseFragment(), KodeinAware, OnLanguageSelectedListener {
 
     override lateinit var kodein: Kodein
 
@@ -33,18 +38,55 @@ class TranslateFragment : BaseFragment(), KodeinAware {
         super.onViewCreated(view, savedInstanceState)
 
         initializeViews()
-        text_for_translation.afterTextChanged {
-            viewModel.observeTranslation("en-ru", it)
-        }
+
         viewModel.translationsList.observe(this, Observer {
             translation_group.removeAllViews()
             it.translations.forEach { createChip(it) }
         })
+        viewModel.fromLanguage.observe(this, Observer {
+            from_language.text = it.toUpperCase()
+        })
+        viewModel.toLanguage.observe(this, Observer {
+            to_language.text = it.toUpperCase()
+        })
+        viewModel.errors.observe(this, Observer {
+            Snackbar.make(view, resources.getString(R.string.translate_screen_error_translate), Snackbar.LENGTH_SHORT)
+                .show()
+        })
+    }
+
+    override fun selectLanguage(language: String, code: Int) {
+        when (code) {
+            FROM_LANGUAGE_CODE -> {
+                translation_group.removeAllViews()
+                viewModel.fromLanguage.value = LanguageHelper.map[language]?.toLowerCase()
+            }
+            TO_LANGUAGE_CODE -> {
+                translation_group.removeAllViews()
+                viewModel.toLanguage.value = LanguageHelper.map[language]?.toLowerCase()
+            }
+        }
+        if (text_for_translation.editableText.isNotEmpty()) {
+            viewModel.observeTranslation(text_for_translation.text.toString())
+        }
     }
 
     private fun initializeViews() {
         viewModel = ViewModelProviders.of(requireActivity(), viewModelFactory)
             .get(TranslateViewModel::class.java)
+
+        val chooseLanguageDialog = ChooseLanguageDialog()
+        from_language.setOnClickListener {
+            chooseLanguageDialog.setTargetFragment(this, FROM_LANGUAGE_CODE)
+            chooseLanguageDialog.show(fragmentManager, ChooseLanguageDialog.TAG)
+        }
+        to_language.setOnClickListener {
+            chooseLanguageDialog.setTargetFragment(this, TO_LANGUAGE_CODE)
+            chooseLanguageDialog.show(fragmentManager, ChooseLanguageDialog.TAG)
+        }
+        text_for_translation.afterTextChanged {
+            viewModel.observeTranslation(it)
+        }
     }
 
     private fun createChip(translation: Translation) {
--- presentation/src/main/java/com/spinkevich/wordkeeper/feature/translate/TranslateViewModel.kt
+++ presentation/src/main/java/com/spinkevich/wordkeeper/feature/translate/TranslateViewModel.kt
@@ -4,29 +4,34 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.spinkevich.domain.model.TranslationModel
 import com.spinkevich.domain.usecase.TranslateUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class TranslateViewModel(
     private val translateUseCase: TranslateUseCase
 ) : ViewModel() {
 
     val translationsList: MutableLiveData<TranslationModel> = MutableLiveData()
+    val errors: MutableLiveData<Exception> = MutableLiveData()
+
+    var fromLanguage: MutableLiveData<String> = MutableLiveData("en")
+    var toLanguage: MutableLiveData<String> = MutableLiveData("ru")
 
     private val viewModelJob = SupervisorJob()
 
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    fun observeTranslation(direction: String, text: String) {
+    fun observeTranslation(text: String) {
+        val direction = "${fromLanguage.value}-${toLanguage.value}"
         uiScope.launch {
-            var translations: TranslationModel? = null
-            withContext(Dispatchers.IO) {
-                translations = translateUseCase.translate(direction, text)
+            try {
+                var translations: TranslationModel? = null
+                withContext(Dispatchers.IO) {
+                    translations = translateUseCase.translate(direction, text)
+                }
+                translationsList.value = translations
+            } catch (ex: Exception) {
+                errors.value = ex
             }
-            translationsList.value = translations
         }
     }
 
